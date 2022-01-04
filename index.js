f3a78dd10231d0f3e648bcdee636c8f54df9d356
Jennifer Jimenez <jenniferjimenez90@gmail.com>
2022-01-04 15:17:10 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function superbowlWin(record){
+let winYear =  record.find(element => element.result === "W")
+return winYear? winYear.year: undefined;
+ }
+
+
+ function superbowlWin(array) {
+     for (let game of array){
+         if(game.result === "W"){
+             return game.year;
+         }
+     }
+ }
+ console.log(superbowlWin(record));
